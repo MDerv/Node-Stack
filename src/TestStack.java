524328d49fb423de6a74f70046e62df7674d7c09
MDerv <dsharma21@kentdenver.org>
2020-11-02 01:08:46 -0700

    I codded the StackNode class with methods to push, pop, check if the stack is empty, get size, and peek.

---
+++ src/TestStack.java
@@ -0,0 +1,12 @@
+/*
+Dhruv Sharma
+10/28/2020
+11/2/2020
+This is a the class that defines a stack using nodes.
+ */
+
+public class TestStack {
+    public static void main(String[] args) {
+
+    }
+}
